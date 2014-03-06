976cab7fef462e3eed1f7285639ac66050c06b3d
Brandon Bickford <bickfordb@gmail.com>
2014-03-06 10:08:53 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# migratory
+
+Clojure migration library
+
+## Usage
+
+
+## License
+
+Copyright 2014 Brandon Bickford
+
+Distributed under the GNU GPL V3 or later
+
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject migratory "0.1.0"
+  :description "FIXME: write description"
+  :url "http://github.com/bickfordb/migratory"
+  :license {:name "GPL V3"
+            :url "http://www.gnu.org"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
